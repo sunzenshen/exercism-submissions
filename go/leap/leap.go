96bd76a8b8245857b91e17c8087f71bc3476a64c
Alan Shen <sunzenshen@gmail.com>
2014-07-24 01:19:39 -0600

    Style experiment with removing comments

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,13 +2,17 @@ package leap
 
 // IsLeapYear returns whether the input year is a leap year.
 func IsLeapYear(year int) bool {
-	if (year%4 == 0) && (year%100 != 0) {
-		// A leap year occurs on very year that is evenly divisible by 4
-		//   except when the year is also evenly divisible by 100
+	if (year%4 == 0) && !isInvalidCentury(year) {
 		return true
-	} else if year%400 == 0 {
-		// Exception of the exception. Divisibility tests for 4 and 100 implicit.
-		return true
+	}
+	return false
+}
+
+func isInvalidCentury(year int) bool {
+	if year%100 == 0 {
+		if year%400 != 0 {
+			return true
+		}
 	}
 	return false
 }
