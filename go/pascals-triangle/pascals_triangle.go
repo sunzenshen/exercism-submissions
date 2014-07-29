df70a8de660d4ce53080d0bbf539ba6f875ab8ec
Alan Shen <sunzenshen@gmail.com>
2014-07-29 00:16:19 -0600

    go/pascals-triangle first iteration

---
+++ go/pascals-triangle/pascals_triangle.go
@@ -0,0 +1,15 @@
+package pascal
+
+// Triangle returns Pascal's triangle for a given number of rows
+func Triangle(numRows int) [][]int {
+	triangle := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		triangle[i] = make([]int, i+1)
+		triangle[i][0] = 1
+		triangle[i][i] = 1
+		for j := 1; j < i; j++ {
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+		}
+	}
+	return triangle
+}
