a05c0ef2ed2c93279779b94b38ef55f8ecbf5aa1
Alan Shen <sunzenshen@gmail.com>
2014-08-30 17:14:25 -0600

    Separate upper/lower case checks from isShouting

--- cpp/bob/bob.h
+++ cpp/bob/bob.h
@@ -1,52 +1,56 @@
 #ifndef BOB_H
 #define BOB_H
 
+#include <algorithm>
 #include <cctype>
 #include <string>
+#include <iostream>
 
-namespace bob {
-
-    namespace {
-        bool isShouting(std::string const& statement) {
-            auto ret = false;
-            auto strLen = statement.length();
-            for (auto letter : statement) {
-                if (islower(letter)) {
-                    return false;
-                }
-                if (isalpha(letter)){
-                    ret = true;
-                }
-            }
-            return ret;
-        }
-
-        bool isQuestion(std::string const& statement) {
-            auto lastCharIndex = statement.length() - 1;
-            return statement[lastCharIndex] == '?';
-        }
-
-        bool isSilence(std::string const& statement) {
-            return statement.length() == 0;
-        }
-
-        std::string getTrimmedString(std::string const& statement) {
-            auto firstPos = statement.find_first_not_of(" ");
-            if (firstPos == std::string::npos) {
-                return "";
-            }
-            auto lastPos = statement.find_last_not_of(" ");
-            return statement.substr(firstPos, lastPos-firstPos+1);
-        }
+namespace {
+    bool isUpperCasePresent(std::string const& statement) {
+        return statement.end() !=
+            std::find_if(statement.begin(), statement.end(), isalpha);
     }
 
+    bool isLowerCasePresent(std::string const& statement) {
+        return statement.end() !=
+            std::find_if(statement.begin(), statement.end(), islower);
+    }
+
+    bool isShouting(std::string const& statement) {
+        if (isUpperCasePresent(statement) && !isLowerCasePresent(statement)) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isQuestion(std::string const& statement) {
+        auto lastCharIndex = statement.length() - 1;
+        return statement[lastCharIndex] == '?';
+    }
+
+    std::string getTrimmedString(std::string const& statement) {
+        auto firstPos = statement.find_first_not_of(" ");
+        if (firstPos == std::string::npos) {
+            return "";
+        }
+        auto lastPos = statement.find_last_not_of(" ");
+        return statement.substr(firstPos, lastPos-firstPos+1);
+    }
+
+    bool isSilence(std::string const& statement) {
+        return getTrimmedString(statement).length() == 0;
+    }
+}
+
+
+namespace bob {
     std::string hey(std::string const& statement) {
-        auto testStatement = getTrimmedString(statement);
-        if (isShouting(testStatement)) {
+        if (isShouting(statement)) {
             return "Woah, chill out!";
-        } else if (isQuestion(testStatement)) {
+        } else if (isQuestion(statement)) {
             return "Sure.";
-        } else if (isSilence(testStatement)) {
+        } else if (isSilence(statement)) {
             return "Fine. Be that way!";
         }
         return "Whatever.";
