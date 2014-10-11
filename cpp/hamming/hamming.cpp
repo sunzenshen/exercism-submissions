a3e1e29199b03497c20946bea23e9258084aa31c
Alan Shen <sunzenshen@gmail.com>
2014-10-11 11:18:34 -0600

    Simplify compare length extraction with min()

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "hamming.h"
 
 using namespace std;
@@ -7,11 +8,7 @@ namespace hamming {
 unsigned compute(string a, string b) {
   unsigned distance = 0;
   unsigned compareLen = 0;
-  if (a.length()< b.length()) {
-    compareLen = a.length();
-  } else {
-    compareLen = b.length();
-  }
+  compareLen = min(a.length(), b.length());
   for (unsigned i=0; i<compareLen; ++i) {
     if (a[i] != b[i]) {
       ++distance;
