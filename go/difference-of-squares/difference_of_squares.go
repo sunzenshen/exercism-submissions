fc883512edbe56b40aff4fed6fe605b689b8bf7e
Alan Shen <sunzenshen@gmail.com>
2014-10-21 23:14:48 -0600

    Reduce number of math operations for SquareOfSums
    Oversimplifying the derivation actually resulted in more work for the
    machine. Execute the squared operation with a temporary storage
    variable.

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,7 +2,8 @@ package diffsquares
 
 // SquareOfSums squares the sums the numbers 0...n
 func SquareOfSums(n int) int {
-	return (n*n*n*n + 2*n*n*n + n*n) / 4
+	tmp := n * (n + 1) / 2
+	return tmp * tmp
 }
 
 // SumOfSquares sums the squares of numbers 0...n
