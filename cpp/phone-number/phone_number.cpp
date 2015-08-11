ebb7737ac7103158beac9eed0a3de780cd18f2a1
Alan Shen <sunzenshen@gmail.com>
2015-08-10 23:39:39 -0600

    Stub phone_number constructor

---
+++ cpp/phone-number/phone_number.cpp
@@ -0,0 +1,7 @@
+#include "phone_number.h"
+
+using namespace std;
+
+phone_number::phone_number(string textFormatNumber) {
+  // stub
+}
--- cpp/phone-number/phone_number.h
+++ cpp/phone-number/phone_number.h
@@ -1,8 +1,14 @@
 #ifndef _PHONE_NUMBER_H
 #define _PHONE_NUMBER_H
 
+#include <string>
+
 class phone_number {
+
+public:
+  phone_number(std::string textFormatNumber);
 
 };
 
 #endif
+
