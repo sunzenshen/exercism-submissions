3c45ec1e8394249d43886b9dd69c890a60144589
Alan Shen <sunzenshen@gmail.com>
2015-10-27 00:29:29 -0600

    Stub empty school roster

--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,4 +1,24 @@
 #ifndef _GRADE_SCHOOL_H
 #define _GRADE_SCHOOL_H
 
+#include "map"
+#include "vector"
+
+namespace grade_school
+{
+
+class school
+{
+public:
+  const std::map<int, std::vector<std::string> >& roster() const {
+    return roster_;
+  }
+
+private:
+  std::map<int, std::vector<std::string> > roster_;
+}; // end class school
+
+} // end namespace grade_school
+
 #endif
+
