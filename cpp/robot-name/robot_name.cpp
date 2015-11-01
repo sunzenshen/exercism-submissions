827ce7e26816002ac61a9a420466de2ba87f3be0
Alan Shen <sunzenshen@gmail.com>
2015-11-01 11:59:47 -0700

    Create robot with unchanging stub name

--- cpp/robot-name/robot_name.cpp
+++ cpp/robot-name/robot_name.cpp
@@ -1,5 +1,12 @@
 #include "robot_name.h"
 
 using namespace robot_name;
+using namespace std;
 
-robot::robot() {}
+robot::robot() {
+  name_ = generate_name();
+}
+
+string robot::generate_name() const {
+  return "AB123";
+}
--- cpp/robot-name/robot_name.h
+++ cpp/robot-name/robot_name.h
@@ -1,6 +1,9 @@
 #ifndef ROBOT_NAME_H
 #define ROBOT_NAME_H
 
+#include "map"
+#include "string"
+
 namespace robot_name
 {
 
@@ -8,6 +11,16 @@ class robot
 {
 public:
   robot();
+
+  std::string name() const { return name_; }
+
+private:
+  // Name of individual robot
+  std::string name_;
+  // Track the names used across all robots
+  static std::map<std::string, bool> names_used_;
+  // Robot name generator
+  std::string generate_name() const;
 }; // end class robot
 
 } // end namespace robot_name
