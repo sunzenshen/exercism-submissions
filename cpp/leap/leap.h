40d32318ddd44ec24aadd5e03905516c9db6823d
Alan Shen <sunzenshen@gmail.com>
2015-11-07 00:11:58 -0700

    Encapsulate divisibility check as a function

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -1,10 +1,21 @@
 #ifndef LEAP_H
 #define LEAP_H
 
+namespace
+{
+
+bool is_divisible(int numerator, int divisor) {
+  return numerator % divisor == 0;
+}
+
+}
+
 namespace leap
 {
 
-bool is_leap_year(int year) {return year % 4 == 0;}
+bool is_leap_year(int year) {
+  return is_divisible(year, 4);
+}
 
 } // end namespace leap
 
