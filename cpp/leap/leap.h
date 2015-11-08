bb56da2f3ac3a10e7facc3848cc736382a150256
Alan Shen <sunzenshen@gmail.com>
2015-11-08 13:17:59 -0700

    Introduce is_doubly_even() to check divisibility by 4

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -8,6 +8,10 @@ bool is_divisible(int numerator, int divisor) {
   return numerator % divisor == 0;
 }
 
+bool is_doubly_even(int numerator) {
+  return is_divisible(numerator, 4);
+}
+
 bool is_century(int year) {
   return is_divisible(year, 100);
 }
@@ -18,7 +22,7 @@ namespace leap
 {
 
 bool is_leap_year(int year) {
-  return is_divisible(year, 4) && !is_century(year);
+  return is_doubly_even(year) && !is_century(year);
 }
 
 } // end namespace leap
