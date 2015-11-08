65053a8715e751628d90003bc525fe85f1d6e324
Alan Shen <sunzenshen@gmail.com>
2015-11-08 13:13:13 -0700

    Introduce is_century() for code readability

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -8,13 +8,17 @@ bool is_divisible(int numerator, int divisor) {
   return numerator % divisor == 0;
 }
 
+bool is_century(int year) {
+  return is_divisible(year, 100);
+}
+
 }
 
 namespace leap
 {
 
 bool is_leap_year(int year) {
-  return is_divisible(year, 4) && !is_divisible(year, 100);
+  return is_divisible(year, 4) && !is_century(year);
 }
 
 } // end namespace leap
