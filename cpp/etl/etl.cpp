74d3fdda836da827a03702b164591c9c1918de96
Alan Shen <sunzenshen@gmail.com>
2015-11-14 10:34:30 -0700

    Iterate through score/letter combinations for transform()

--- cpp/etl/etl.cpp
+++ cpp/etl/etl.cpp
@@ -5,9 +5,16 @@ using namespace std;
 namespace etl
 {
 
+// Transform record of Scrabble scores from being score index to letter indexed
 map<char, int> transform(map<int, vector<char> > legacyScores) {
   map<char, int> reformattedScores;
-  reformattedScores['a'] += 1;
+  for (auto lettersPerScore : legacyScores) {
+    int scoreForTheseLetters = lettersPerScore.first;
+    vector<char>& letters = lettersPerScore.second;
+    for (auto letter : letters) {
+      reformattedScores[tolower(letter)] = scoreForTheseLetters;
+    }
+  }
   return reformattedScores;
 }
 
--- cpp/etl/etl_test.cpp
+++ cpp/etl/etl_test.cpp
@@ -28,7 +28,6 @@ BOOST_AUTO_TEST_CASE(transforms_one_value)
     REQUIRE_EQUAL_CONTAINERS(expected, actual);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(transforms_more_values)
 {
     const std::map<int, std::vector<char>> old{{1, {'A', 'E', 'I', 'O', 'U'}}};
@@ -73,4 +72,3 @@ BOOST_AUTO_TEST_CASE(transforms_a_full_dataset)
     };
     REQUIRE_EQUAL_CONTAINERS(expected, actual);
 }
-#endif
