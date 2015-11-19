dea84e259cb08ddf04ca759892f53171b7a7db35
Alan Shen <sunzenshen@gmail.com>
2015-11-19 00:14:00 -0700

    space_age constructor for age in seconds

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -6,7 +6,11 @@ namespace space_age
 
 class space_age
 {
+public:
+  space_age(unsigned int age_in_seconds) : age_in_seconds_(age_in_seconds) {}
 
+private:
+  unsigned int age_in_seconds_;
 }; // end class space_age
 
 } // end namespace space_age
