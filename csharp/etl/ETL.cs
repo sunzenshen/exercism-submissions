e627c93686d1cd65dbac6936a5ab41c4e357960a
Alan Shen <sunzenshen@gmail.com>
2015-11-26 15:16:01 -0700

    Stub ETL:Transform()
    Minimum implementation to pass the first test.

---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public class ETL
+{
+	public static Dictionary<string, int> Transform (Dictionary<int, IList<string>> oldScores)
+	{
+		return new Dictionary<string, int> { { "a", 1 } };
+	}
+}
