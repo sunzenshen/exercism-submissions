dbecf844e339731cc408c53716d741bccdc83d9f
Alan Shen <sunzenshen@gmail.com>
2015-11-25 17:27:03 -0700

    Calculate number of grains for a particular square()

--- cpp/grains/grains.h
+++ cpp/grains/grains.h
@@ -1,11 +1,14 @@
 #ifndef GRAINS_H
 #define GRAINS_H
 
+#include <math.h>
+
 namespace grains
 {
 
+// Returns the number of grains at a given square position
 unsigned long long square(int position) {
-  return 1ULL;
+  return pow(2, position-1);
 }
 
 } // end namespace grains
