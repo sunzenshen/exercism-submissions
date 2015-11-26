cbb2ca3c62e1cd0f1e35de52b350de07d26753f0
Alan Shen <sunzenshen@gmail.com>
2015-11-26 00:06:03 -0700

    Calculate total() number of grains on the board

--- cpp/grains/grains.h
+++ cpp/grains/grains.h
@@ -11,6 +11,15 @@ unsigned long long square(int position) {
   return pow(2, position-1);
 }
 
+// Returns the total number of grains on the board
+unsigned long long total() {
+  unsigned long long sum = 0;
+  for (int i=1; i<=64; ++i) {
+    sum += square(i);
+  }
+  return sum;
+}
+
 } // end namespace grains
 
 #endif
