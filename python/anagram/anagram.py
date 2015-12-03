85c21efe08b931c513d2c2a43a41373e31bd77d5
Alan Shen <sunzenshen@gmail.com>
2015-12-03 00:20:20 -0800

    Match anagram letter counts with key generated by alphabetic sorting

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,4 +1,25 @@
 def detect_anagrams(target, candidates):
     """ Given a target string and a space-separated list of candidates,
         find the anagrams of the target string """
-    return []
+    matches = []
+    for candidate in candidates:
+        if is_anagram(target, candidate):
+            matches.append(candidate)
+    return matches
+
+def is_anagram(target, candidate):
+    """ Returns true if the candidate string is an anagram of the target """
+    if target == candidate:
+        # Identical strings are not anagrams of each other
+        return False
+    elif get_key(target) == get_key(candidate):
+        # The number of letters match, so the two words are anagrams
+        return True
+    else:
+        # Letters on either side don't match up as anagrams
+        return False
+
+def get_key(text):
+    """ Generates a key from a string,
+        which is the alphabetically sorted letters """
+    return sorted(text)
