9ddc8b23c0759b771f5b4c17db9bb2eb34664b7f
Alan Shen <sunzenshen@gmail.com>
2015-12-03 00:56:01 -0800

    Stub etl Transform()

---
+++ go/etl/etl.go
@@ -0,0 +1,7 @@
+package etl
+
+// Transform takes the legacy score format and returns the new score format.
+func Transform(oldScores map[int][]string) map[string]int {
+	newScores := make(map[string]int)
+	return newScores
+}
