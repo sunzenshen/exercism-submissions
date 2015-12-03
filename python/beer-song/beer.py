71522b164067ba9d4edce21391bebb365de3eb61
Alan Shen <sunzenshen@gmail.com>
2015-12-03 00:33:31 -0800

    verse() case: no more bottles of beer

--- python/beer-song/beer.py
+++ python/beer-song/beer.py
@@ -2,4 +2,9 @@ def song(start_num, end_num=0):
     return ""
 
 def verse(num_bottles):
-    return ""
+    """ Recite the n-th verse of the beer song. """
+    if num_bottles == 0:
+        return ("No more bottles of beer on the wall, no more bottles of beer.\n"
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+    else:
+        return ""
