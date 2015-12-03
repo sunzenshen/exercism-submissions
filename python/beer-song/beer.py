4049495f30a5718de3791b56819ead5051c6b9da
Alan Shen <sunzenshen@gmail.com>
2015-12-03 00:35:12 -0800

    verse: 1 bottle of beer

--- python/beer-song/beer.py
+++ python/beer-song/beer.py
@@ -6,5 +6,8 @@ def verse(num_bottles):
     if num_bottles == 0:
         return ("No more bottles of beer on the wall, no more bottles of beer.\n"
                 "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+    elif num_bottles == 1:
+        return ("1 bottle of beer on the wall, 1 bottle of beer.\n"
+                "Take it down and pass it around, no more bottles of beer on the wall.\n")
     else:
         return ""
