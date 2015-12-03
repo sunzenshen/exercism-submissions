2c60721734448f09f211624c6d7b43e850e2cddb
Alan Shen <sunzenshen@gmail.com>
2015-12-03 01:15:31 -0800

    Transform() old scores into new format indexed by letters

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,7 +1,19 @@
 package etl
 
+import "strings"
+
 // Transform takes the legacy score format and returns the new score format.
 func Transform(oldScores map[int][]string) map[string]int {
 	newScores := make(map[string]int)
+	for score, letters := range oldScores {
+		for _, letter := range letters {
+			lowCasedLetter := strings.ToLower(letter)
+			alreadyEnteredScore, exists := newScores[lowCasedLetter]
+			if exists && (alreadyEnteredScore != score) {
+				panic("Encountered a contradicting score entry for letter: " + letter)
+			}
+			newScores[lowCasedLetter] = score
+		}
+	}
 	return newScores
 }
