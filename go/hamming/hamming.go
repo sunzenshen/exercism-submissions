35b33880868a8c2357953ce7f5417964f3e8ffd8
Alan Shen <sunzenshen@gmail.com>
2015-12-03 16:56:27 -0800

    Count hamming Distance() of input strings

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -5,5 +5,16 @@ const TestVersion = 2
 
 // Distance returns the Hamming distance between two given DNA string representations
 func Distance(strandA, strandB string) (int, error) {
-	return 0, nil
+	var distance int = 0
+	// Any extra nucleotides at the end of either strand will be ignored
+	numCompare := len(strandA)
+	if len(strandB) < numCompare {
+		numCompare = len(strandB)
+	}
+	for i := 0; i < numCompare; i++ {
+		if strandA[i] != strandB[i] {
+			distance++
+		}
+	}
+	return distance, nil
 }
