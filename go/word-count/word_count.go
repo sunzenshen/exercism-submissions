f070e5b6d6c5d37a0b466845e2256abbc199c476
Alan Shen <sunzenshen@gmail.com>
2015-12-03 21:34:15 -0800

    Count the words from a given phrase
    Does not yet handle punctuation or mixed casing

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,5 +1,7 @@
 package wordcount
 
+import "strings"
+
 // TestVersion specifies which exercism test set this exercise is submitted for
 const TestVersion = 1
 
@@ -8,6 +10,10 @@ type Frequency map[string]int
 
 // WordCount takes a string and returns a dictionary tracking the counts of individual words
 func WordCount(phrase string) Frequency {
-	var counts Frequency
+	var counts = make(Frequency)
+	words := strings.Fields(phrase)
+	for _, word := range words {
+		counts[word] = counts[word] + 1
+	}
 	return counts
 }
