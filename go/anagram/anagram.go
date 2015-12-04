a5fc5786a677af9455a86631f7ceb25152412ed0
Alan Shen <sunzenshen@gmail.com>
2015-12-03 17:14:55 -0800

    Stub Detect() anagrams
    Returns no matches to pass the null test

---
+++ go/anagram/anagram.go
@@ -0,0 +1,7 @@
+package anagram
+
+// Detect finds the anagrams for a given word, from an input list of candidate strings
+func Detect(target string, candidates []string) []string {
+	var matches []string
+	return matches
+}
