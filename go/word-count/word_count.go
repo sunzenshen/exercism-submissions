ee552429f3c6bfab3b55aeaa90cd0ec94f897823
Alan Shen <sunzenshen@gmail.com>
2015-12-03 21:38:47 -0800

    Strip punctuation from phrase before counting words

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -11,9 +11,23 @@ type Frequency map[string]int
 // WordCount takes a string and returns a dictionary tracking the counts of individual words
 func WordCount(phrase string) Frequency {
 	var counts = make(Frequency)
+	phrase = stripNonAlphaNum(phrase)
 	words := strings.Fields(phrase)
 	for _, word := range words {
 		counts[word] = counts[word] + 1
 	}
 	return counts
+}
+
+func stripNonAlphaNum(phrase string) string {
+	keepAlphaNum := func(r rune) rune {
+		if (r >= 'A' && r <= 'Z') ||
+			(r >= 'a' && r <= 'z') ||
+			(r >= '0' && r <= '9') ||
+			(r == ' ') {
+			return r
+		}
+		return -1
+	}
+	return strings.Map(keepAlphaNum, phrase)
 }
