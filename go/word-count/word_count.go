1c8c6f545af23d4e116ca081b67f6f131ca0839f
Alan Shen <sunzenshen@gmail.com>
2015-12-03 21:13:22 -0800

    Stub WordCount()

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,11 +1,13 @@
-// +build !example
-
 package wordcount
 
+// TestVersion specifies which exercism test set this exercise is submitted for
 const TestVersion = 1
 
-// Use this return type.
+// Frequency records the frequency of words seen in a given string
 type Frequency map[string]int
 
-// Just implement the function.
-func WordCount(phrase string) Frequency
+// WordCount takes a string and returns a dictionary tracking the counts of individual words
+func WordCount(phrase string) Frequency {
+	var counts Frequency
+	return counts
+}
