30e006854a1fdcf87e161e172e72f4717bf1868d
Alan Shen <sunzenshen@gmail.com>
2015-12-03 16:53:19 -0800

    Stub Distance()

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,9 @@
-// +build !example
-
 package hamming
 
+// TestVersion defined for the provided exercism test cases
 const TestVersion = 2
+
+// Distance returns the Hamming distance between two given DNA string representations
+func Distance(strandA, strandB string) (int, error) {
+	return 0, nil
+}
