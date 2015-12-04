79f3cc3d25bb9babf252bdeeed7651b8dfa011b0
Alan Shen <sunzenshen@gmail.com>
2015-12-03 17:07:18 -0800

    Return error if input nucleotide strings do not have matching length

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,16 +1,18 @@
 package hamming
 
+import "errors"
+
 // TestVersion defined for the provided exercism test cases
 const TestVersion = 2
 
 // Distance returns the Hamming distance between two given DNA string representations
 func Distance(strandA, strandB string) (int, error) {
-	var distance int = 0
-	// Any extra nucleotides at the end of either strand will be ignored
-	numCompare := len(strandA)
-	if len(strandB) < numCompare {
-		numCompare = len(strandB)
+	var distance int
+	// Return an error if strings are not of the same length
+	if len(strandA) != len(strandB) {
+		return -1, errors.New("distance: input strings are not of the same length")
 	}
+	numCompare := len(strandA)
 	for i := 0; i < numCompare; i++ {
 		if strandA[i] != strandB[i] {
 			distance++
