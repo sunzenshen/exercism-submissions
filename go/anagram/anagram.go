70e106708359a4fabfc86ab3f6c41c0bd67d960f
Alan Shen <sunzenshen@gmail.com>
2015-12-03 19:33:49 -0800

    Detect anagrams by matching keys of sorted letters
    Does not yet detect anagrams of different casing

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,7 +1,41 @@
 package anagram
 
+import "sort"
+import "strings"
+
 // Detect finds the anagrams for a given word, from an input list of candidate strings
 func Detect(target string, candidates []string) []string {
 	var matches []string
+	for _, candidate := range candidates {
+		if areAnagrams(target, candidate) {
+			matches = append(matches, strings.ToLower(candidate))
+		}
+	}
 	return matches
+}
+
+func areAnagrams(a, b string) bool {
+	if sortedByRune(a) != sortedByRune(b) {
+		// Letter counts do not match for anagram candidates
+		return false
+	}
+	return true
+}
+
+// runeSlice is defined for sorting string alphabetically
+type runeSlice []rune
+
+// Len returns the number of runes that make up a given string.
+func (s runeSlice) Len() int { return len(s) }
+
+// Less returns whether the first rune is a lower order of the second rune.
+func (s runeSlice) Less(i, j int) bool { return s[i] < s[j] }
+
+// Swap takes two slice indices and swaps the containing runes.
+func (s runeSlice) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+func sortedByRune(s string) string {
+	runes := []rune(s)
+	sort.Sort(runeSlice(runes))
+	return string(runes)
 }
