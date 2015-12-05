ff02b4310de42ad872062140ea8d6c3576ceeaf7
Alan Shen <sunzenshen@gmail.com>
2015-12-05 14:00:54 -0800

    Trim unused whitespace from greeting

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -7,6 +7,8 @@ const TestVersion = 1
 
 // Hey determines the reaction Bob has to a given greeting.
 func Hey(speech string) string {
+	// Trim unused characters from the greeting
+	speech = strings.Trim(speech, " ")
 	if (speech == strings.ToUpper(speech)) && (speech != strings.ToLower(speech)) {
 		// Yelling
 		return "Whoa, chill out!"
