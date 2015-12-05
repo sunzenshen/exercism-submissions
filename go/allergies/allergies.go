b8ba1b1639ad968ec85e57575f60805db486b254
Alan Shen <sunzenshen@gmail.com>
2015-12-04 18:14:32 -0800

    Stub Allergies() and AllergicTo()

---
+++ go/allergies/allergies.go
@@ -0,0 +1,12 @@
+package allergies
+
+// Allergies takes a score and returns all allergies included.
+func Allergies(score int) []string {
+	var allergens []string
+	return allergens
+}
+
+// AllergicTo determines if a score matches a given allergen.
+func AllergicTo(score int, allergen string) bool {
+	return false
+}
