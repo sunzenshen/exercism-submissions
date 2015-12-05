5937241faa2e7b04b14871e7f8b49efd291895aa
Alan Shen <sunzenshen@gmail.com>
2015-12-05 14:12:58 -0800

    Stub ToRna()

---
+++ go/rna-transcription/rna_transcription.go
@@ -0,0 +1,9 @@
+package strand
+
+// TestVersion indicates which exercism test suite to use
+const TestVersion = 1
+
+// ToRna coverts an input DNA strand to its RNA complement.
+func ToRna(dna string) string {
+	return "G"
+}
