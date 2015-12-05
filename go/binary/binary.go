1dddc1fe39cebe55fc6194033b2f9995f4b9c914
Alan Shen <sunzenshen@gmail.com>
2015-12-04 18:06:34 -0800

    ParseBinary(string) into decimal value

--- go/binary/binary.go
+++ go/binary/binary.go
@@ -1,7 +1,21 @@
 package binary
 
+import "errors"
+import "math"
+
 // ParseBinary takes a binary number represented by a string,
 // and returns the decimal value.
-func ParseBinary(string) (int, error) {
-	return 1, nil
+func ParseBinary(binary string) (int, error) {
+	var decimal int
+	numDigits := len(binary)
+	power := 0
+	for i := numDigits - 1; i >= 0; i, power = i-1, power+1 {
+		if binary[i] == '1' {
+			decimal = decimal + int(math.Pow(float64(2), float64(power)))
+		} else if binary[i] != '0' {
+			// Encountered a rune that cannot possibly be from a binary number
+			return -1, errors.New("binary string input had invalid character that was not 0 or 1")
+		}
+	}
+	return decimal, nil
 }
