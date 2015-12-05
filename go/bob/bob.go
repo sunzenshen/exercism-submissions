124b4563636c8dd43918e3d1bad6597ac5dcf717
Alan Shen <sunzenshen@gmail.com>
2015-12-05 13:57:27 -0800

    Bob gets excited by yelling

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,9 +1,15 @@
 package bob
 
+import "strings"
+
 // TestVersion defines which exercism test set to run on this implementation
 const TestVersion = 1
 
 // Hey determines the reaction Bob has to a given greeting.
 func Hey(speech string) string {
+	if (speech == strings.ToUpper(speech)) && (speech != strings.ToLower(speech)) {
+		// Yelling
+		return "Whoa, chill out!"
+	}
 	return "Whatever."
 }
