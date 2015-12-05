1544064eea273a93fd28dfd775c4d9b1b928669a
Alan Shen <sunzenshen@gmail.com>
2015-12-05 15:54:18 -0800

    Translate DNA in ToRna()

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,9 +1,24 @@
 package strand
 
+import "strings"
+
 // TestVersion indicates which exercism test suite to use
 const TestVersion = 1
 
 // ToRna coverts an input DNA strand to its RNA complement.
 func ToRna(dna string) string {
-	return "G"
+	rnaTranscription := func(nucleotide rune) rune {
+		switch nucleotide {
+		case 'G':
+			return 'C'
+		case 'C':
+			return 'G'
+		case 'T':
+			return 'A'
+		case 'A':
+			return 'U'
+		}
+		panic("Invalid DNA sequence provided... Input: " + dna)
+	}
+	return strings.Map(rnaTranscription, dna)
 }
