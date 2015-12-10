68e99896ae17edada0d4771169865e7182313132
Alan Shen <sunzenshen@gmail.com>
2015-12-09 19:18:21 -0800

    Throw exception if DNA strands are not of the same length

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,6 +4,10 @@ public class Hamming
 {
 	public static int Compute (string strandA, string strandB)
 	{
+		if (strandA.Length != strandB.Length) {
+			throw new ArgumentException ("Input DNA strands are not of the same length");
+		}
+
 		return 0;
 	}
 }
