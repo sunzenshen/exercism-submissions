dc4b908b2498eed3cea41c03d9f7178cb2b30081
Alan Shen <sunzenshen@gmail.com>
2015-12-10 11:18:12 -0800

    Count strand differences for Hamming distance

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,10 +4,18 @@ public class Hamming
 {
 	public static int Compute (string strandA, string strandB)
 	{
+		int distance = 0;
 		if (strandA.Length != strandB.Length) {
 			throw new ArgumentException ("Input DNA strands are not of the same length");
 		}
 
-		return 0;
+		for (int i = strandA.Length - 1; i >= 0; --i) {
+			if (strandA [i] != strandB [i]) {
+				++distance;
+			}
+				
+		}
+
+		return distance;
 	}
 }
--- csharp/hamming/HammingTest.cs
+++ csharp/hamming/HammingTest.cs
@@ -9,35 +9,30 @@ public class HammingTest
         Assert.That(Hamming.Compute("",""), Is.EqualTo(0));
     }
 
-    [Ignore]
     [Test]
     public void No_difference_between_identical_strands()
     {
         Assert.That(Hamming.Compute("GGACTGA","GGACTGA"), Is.EqualTo(0));
     }
 
-    [Ignore]
     [Test]
     public void Complete_hamming_distance_in_small_strand()
     {
         Assert.That(Hamming.Compute("ACT","GGA"), Is.EqualTo(3));
     }
 
-    [Ignore]
     [Test]
     public void Hamming_distance_is_off_by_one_strand()
     {
         Assert.That(Hamming.Compute("GGACGGATTCTG","AGGACGGATTCT"), Is.EqualTo(9));
     }
 
-    [Ignore]
     [Test]
     public void Smalling_hamming_distance_in_middle_somewhere()
     {
         Assert.That(Hamming.Compute("GGACG","GGTCG"), Is.EqualTo(1));
     }
 
-    [Ignore]
     [Test]
     public void Larger_distance()
     {
