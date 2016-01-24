f4c982968a69a0af8a67dd7a6b1e9900582d78d4
Alan Shen <sunzenshen@gmail.com>
2016-01-24 14:44:35 -0700

    Feedback on iteration #2

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,30 +1,35 @@
-﻿public class Bob
-{
-	public static string Hey(string greeting)
-	{
-		if (greeting == null) {
-			throw new System.ArgumentException("Parameter in Bob::Hey() cannot be null", "greeting");;
-		}
+﻿using System;
 
-		// Trim whitespace
-		greeting = greeting.Trim ();
-		// Determine Bob's response
-		if (greeting.Length == 0) {               // silence
-			return "Fine. Be that way!";
-		} else if (IsYelling (greeting)) {        // yelling
-			return "Whoa, chill out!";
-		} else if (greeting.EndsWith ("?")) {     // question
-			return "Sure.";
-		}
+public class Bob
+{
+	public static string Hey(string wordsSpoken)
+	{
+		if (IsSilence (wordsSpoken)) return "Fine. Be that way!";
+
+		if (IsYelling (wordsSpoken)) return "Whoa, chill out!";
+
+		if (IsQuestion (wordsSpoken)) return "Sure.";
+
 		return "Whatever.";
+	}
+
+	private static bool IsQuestion(string str)
+	{
+		return str.Trim().EndsWith ("?");
+	}
+
+	private static bool IsSilence(string str)
+	{
+		return String.IsNullOrWhiteSpace (str);
 	}
 
 	private static bool IsYelling(string str)
 	{
-		if (str.ToLower () == str) {
+		var trimmedStr = str.Trim ();
+		if (trimmedStr.ToLower () == trimmedStr) {
 			// letters are all lower case, doesn't count as yelling
 			return false;
-		} else if (str.ToUpper () == str) {
+		} else if (trimmedStr.ToUpper () == trimmedStr) {
 			// ALL UPPER CASE LETTERS INDICATE YELLING
 			return true;
 		} else {
