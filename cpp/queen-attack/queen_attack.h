88109304408fcfbb9a6162aa82e7ca64b2f84d67
Alan Shen <sunzenshen@gmail.com>
2017-02-19 12:15:51 -0700

    Let's start to be more explicit with private declarations

--- cpp/queen-attack/queen_attack.h
+++ cpp/queen-attack/queen_attack.h
@@ -11,9 +11,6 @@ typedef std::pair<int, int> coordinate;
 
 class chess_board
 {
-  const coordinate m_black = std::make_pair(7, 3);
-  const coordinate m_white = std::make_pair(0, 3);
-
 public:
   chess_board() {}
   chess_board(coordinate white, coordinate black) : m_white(white), m_black(black)
@@ -28,6 +25,10 @@ public:
   operator std::string() const;
 
   bool can_attack() const;
+
+private:
+  const coordinate m_black = std::make_pair(7, 3);
+  const coordinate m_white = std::make_pair(0, 3);
 
 };
 
