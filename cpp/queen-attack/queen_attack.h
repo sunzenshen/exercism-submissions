077d1f466d612b5b7ac800c8428d5afabeaf2863
Alan Shen <sunzenshen@gmail.com>
2017-02-19 10:45:35 -0700

    Default board setup for default constructor

--- cpp/queen-attack/queen_attack.h
+++ cpp/queen-attack/queen_attack.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <utility>
+
+namespace queen_attack
+{
+
+class chess_board
+{
+  const std::pair<int, int> m_black = std::make_pair(7, 3);
+  const std::pair<int, int> m_white = std::make_pair(0, 3);
+
+public:
+  chess_board() {}
+
+  std::pair<int, int> black() const { return m_black; }
+  std::pair<int, int> white() const { return m_white; }
+
+};
+
+} // end namespace queen_attack
