acc955a18ffcc425a0f0cb3a33735e93c6aec67a
Alan Shen <sunzenshen@gmail.com>
2017-02-20 11:37:05 -0700

    Rudimentary start for hex digit conversion

--- cpp/hexadecimal/hexadecimal.cpp
+++ cpp/hexadecimal/hexadecimal.cpp
@@ -1,8 +1,23 @@
 #include "hexadecimal.h"
+#include <cctype>
 
 using namespace std;
 
+namespace
+{
+  int digitHexToDec(char hex)
+  {
+    if (isdigit(hex))
+      return hex - '0';
+
+    // TODO: catch invalid hex characters
+    return tolower(hex) - 'a' + 10;
+  }
+
+} // end anonymous namespace
+
 int hexadecimal::convert(std::string hex)
 {
-  return 1;
+  // TODO: process the other characters in the hex string
+  return digitHexToDec(hex[0]);
 }
