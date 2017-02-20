90e01e2d4a3dda165c416d5146776b25009ded56
Alan Shen <sunzenshen@gmail.com>
2017-02-20 12:04:23 -0700

    Reorder hex digit type checks to be positive-truth-oriented

--- cpp/hexadecimal/hexadecimal.cpp
+++ cpp/hexadecimal/hexadecimal.cpp
@@ -9,19 +9,25 @@ namespace
 {
   int digitHexToDec(char hex)
   {
-    if (isdigit(hex)) {
+    if (isalpha(hex))
+    {
+      // process alpha characters
+      const auto alphaOffset = tolower(hex) - 'a' + 10;
+      if (alphaOffset > 16)
+      {
+        throw domain_error("A hex digit to convert is an alpha that's outside the range A-F.");
+      }
+      return alphaOffset;
+    }
+    else if (isdigit(hex))
+    {
       return hex - '0';
     }
-    else if (not isalpha(hex)) {
+    else
+    {
       throw domain_error("A hex digit to convert is not alpha/numeric.");
     }
 
-    // process alpha characters
-    const auto alphaOffset = tolower(hex) - 'a' + 10;
-    if (alphaOffset > 16) {
-      throw domain_error("A hex digit to convert is an alpha that's outside the range A-F.");
-    }
-    return alphaOffset;
   }
 
 } // end anonymous namespace
